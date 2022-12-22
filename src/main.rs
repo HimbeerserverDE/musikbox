015f73a2a9c4c636393e768c0f2a15916e155ac7
HimbeerserverDE <himbeerserverde@gmail.com>
2022-12-22 19:34:38 +0100

    initial file listing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use std::fs;
 use std::io;
 use std::thread;
 use std::time::Duration;
-use tui::widgets::{Block, Borders};
+use tui::widgets::{Block, Borders, List, ListItem};
 use tui::{backend::CrosstermBackend, Terminal};
 
 fn main() -> anyhow::Result<()> {
@@ -14,9 +15,24 @@ fn main() -> anyhow::Result<()> {
 
     terminal.draw(|f| {
         let size = f.size();
-        let block = Block::default().borders(Borders::ALL);
 
-        f.render_widget(block, size);
+        let files: Vec<ListItem> = fs::read_dir("/home/himbeer/music")
+            .expect("can't read music directory")
+            .map(|entry| {
+                let file = entry
+                    .expect("faulty DirEntry")
+                    .file_name()
+                    .into_string()
+                    .expect("file name contains invalid unicode");
+
+                ListItem::new(file)
+            })
+            .collect();
+
+        let block = Block::default().title("Files").borders(Borders::ALL);
+        let listing = List::new(files).block(block);
+
+        f.render_widget(listing, size);
     })?;
 
     thread::sleep(Duration::from_secs(3));
