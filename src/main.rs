9fe5a9413266057dfae055ab3a561dcb8ad3500f
HimbeerserverDE <himbeerserverde@gmail.com>
2022-12-22 19:15:39 +0100

    initial frame

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use std::io;
+use std::thread;
+use std::time::Duration;
+use tui::widgets::{Block, Borders};
+use tui::{backend::CrosstermBackend, Terminal};
+
+fn main() -> anyhow::Result<()> {
+    enable_raw_mode()?;
+
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default().borders(Borders::ALL);
+
+        f.render_widget(block, size);
+    })?;
+
+    thread::sleep(Duration::from_secs(3));
+
+    disable_raw_mode()?;
+
+    Ok(())
 }
